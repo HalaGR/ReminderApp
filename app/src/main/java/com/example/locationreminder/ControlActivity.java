93776549f7ef3d4c5057d3cb28f48e062cb135b6
joumanakh <joumana.1444@gmail.com>
2022-08-17 16:45:28 +0300

    added-gas station,restaurants,coffee,ATM,hospital

--- app/src/main/java/com/example/locationreminder/ControlActivity.java
+++ app/src/main/java/com/example/locationreminder/ControlActivity.java
@@ -8,6 +8,7 @@ import android.location.Criteria;
 import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Parcelable;
 import android.Manifest;
@@ -20,6 +21,9 @@ import androidx.core.content.ContextCompat;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -31,8 +35,16 @@ import com.google.android.material.snackbar.Snackbar;
 import android.location.Address;
 
 
+import org.json.JSONException;
+import org.json.JSONObject;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.HashMap;
 import java.util.List;
 
 public final class ControlActivity extends SupportActivity implements OnMapReadyCallback{
@@ -59,7 +71,7 @@ public final class ControlActivity extends SupportActivity implements OnMapReady
     private static final String EXTRA_LAT_LNG = "EXTRA_LAT_LNG";
     //public static final MainActivity.Companion Companion = new MainActivity.Companion((DefaultConstructorMarker)null);
     //private Object CameraUpdateFactory;
-
+    FusedLocationProviderClient fusedLocationProviderClient;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_control);
@@ -67,6 +79,13 @@ public final class ControlActivity extends SupportActivity implements OnMapReady
         mapFragment.getMapAsync((OnMapReadyCallback) this);
         findViewById(R.id.newReminder).setVisibility(View.GONE);
         findViewById(R.id.currentLocation).setVisibility(View.GONE);
+
+        findViewById(R.id.gasStation).setVisibility(View.GONE);
+        findViewById(R.id.restaurant).setVisibility(View.GONE);
+        findViewById(R.id.coffee).setVisibility(View.GONE);
+        findViewById(R.id.hospital).setVisibility(View.GONE);
+        findViewById(R.id.atm).setVisibility(View.GONE);
+
         ((FloatingActionButton) findViewById(R.id.newReminder)).setOnClickListener((OnClickListener) (new OnClickListener() {
             @Override
             public void onClick(View it) {
@@ -199,6 +218,12 @@ public final class ControlActivity extends SupportActivity implements OnMapReady
             this.map.setMyLocationEnabled(true);
             findViewById(R.id.newReminder).setVisibility(View.VISIBLE);
             findViewById(R.id.currentLocation).setVisibility(View.VISIBLE);
+
+            findViewById(R.id.gasStation).setVisibility(View.VISIBLE);
+            findViewById(R.id.restaurant).setVisibility(View.VISIBLE);
+            findViewById(R.id.coffee).setVisibility(View.VISIBLE);
+            findViewById(R.id.hospital).setVisibility(View.VISIBLE);
+            findViewById(R.id.atm).setVisibility(View.VISIBLE);
             (findViewById(R.id.currentLocation)).setOnClickListener((OnClickListener) (new OnClickListener() {
                 @Override
                 public void onClick(View it) {
@@ -225,12 +250,102 @@ public final class ControlActivity extends SupportActivity implements OnMapReady
 
 
             }));
+           fusedLocationProviderClient= LocationServices.getFusedLocationProviderClient(this);
+            String url="https://maps.googleapis.com/maps/api/place/nearbysearch/json";
+            String googleMapKey="AIzaSyDpaldh_UjIjQI3XGBYLZpKFzz1uITynU8";
+            //****************************************places gas station,restaurants,coffee,ATM,hospital-start*******
+            (findViewById(R.id.gasStation)).setOnClickListener((OnClickListener) (new OnClickListener() {
+                @Override
+                public void onClick(View it) {
+                    //*************current location-start***********
+                    LatLng latLng=getCurrentLocation();
+                    //*************current location-end***********
+                   String url="https://maps.googleapis.com/maps/api/place/nearbysearch/json"+"?location="+latLng.latitude+","+latLng.longitude+"&radius=5000+"+"&types="+"gas_station"+"&sensor=true"+"&key="+googleMapKey;
+                new PlaceTask().execute(url);
+                }
+
+
+            }));
+            (findViewById(R.id.restaurant)).setOnClickListener((OnClickListener) (new OnClickListener() {
+                @Override
+                public void onClick(View it) {
+                    //*************current location-start***********
+                    LatLng latLng=getCurrentLocation();
+                    //*************current location-end***********
+                    String url="https://maps.googleapis.com/maps/api/place/nearbysearch/json"+"?location="+latLng.latitude+","+latLng.longitude+"&radius=5000+"+"&types="+"restaurant"+"&sensor=true"+"&key="+googleMapKey;
+                    new PlaceTask().execute(url);
+                }
+
+
+            }));
+            (findViewById(R.id.coffee)).setOnClickListener((OnClickListener) (new OnClickListener() {
+                @Override
+                public void onClick(View it) {
+                    //*************current location-start***********
+                    LatLng latLng=getCurrentLocation();
+                    //*************current location-end***********
+                    String url="https://maps.googleapis.com/maps/api/place/nearbysearch/json"+"?location="+latLng.latitude+","+latLng.longitude+"&radius=5000+"+"&types="+"cafe"+"&sensor=true"+"&key="+googleMapKey;
+                    new PlaceTask().execute(url);
+                }
+
+
+            }));
+            (findViewById(R.id.hospital)).setOnClickListener((OnClickListener) (new OnClickListener() {
+                @Override
+                public void onClick(View it) {
+                    //*************current location-start***********
+                    LatLng latLng=getCurrentLocation();
+                    //*************current location-end***********
+                    String url="https://maps.googleapis.com/maps/api/place/nearbysearch/json"+"?location="+latLng.latitude+","+latLng.longitude+"&radius=5000+"+"&types="+"hospital"+"&sensor=true"+"&key="+googleMapKey;
+                    new PlaceTask().execute(url);
+                }
+
+
+            }));
+            (findViewById(R.id.atm)).setOnClickListener((OnClickListener) (new OnClickListener() {
+                @Override
+                public void onClick(View it) {
+                    //*************current location-start***********
+                    LatLng latLng=getCurrentLocation();
+                    //*************current location-end***********
+                    String url="https://maps.googleapis.com/maps/api/place/nearbysearch/json"+"?location="+latLng.latitude+","+latLng.longitude+"&radius=5000+"+"&types="+"ATM"+"&sensor=true"+"&key="+googleMapKey;
+                    new PlaceTask().execute(url);
+                }
+
+
+            }));
+            //****************************************places gas station,restaurants,coffee,ATM,hospital-end*******
+
             this.viewLocations();
             this.centerCamera();
 
 
         }
 
+    }
+    public LatLng getCurrentLocation()
+    {
+        LatLng latLng=new LatLng(0,0);
+        String bestProvider = locationManager.getBestProvider(new Criteria(), false);
+
+        if (ActivityCompat.checkSelfPermission(ControlActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(ControlActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+
+        }
+        Location location = locationManager.getLastKnownLocation(String.valueOf(bestProvider));
+        if (location != null) {
+            latLng = new LatLng(location.getLatitude(), location.getLongitude());
+
+            ControlActivity.this.map.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15.0F));
+
+        }
+        return latLng;
     }
 
 
@@ -267,6 +382,69 @@ public final class ControlActivity extends SupportActivity implements OnMapReady
     }
 
 
+    private class PlaceTask extends AsyncTask<String,Integer,String> {
+        @Override
+        protected String doInBackground(String... strings) {
+            String data= null;
+            try {
+                data= downloadUrl(strings[0]);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return data;
+        }
 
+        @Override
+        protected void onPostExecute(String s) {
+           new ParserTask().execute(s);
+        }
+    }
 
+    private String downloadUrl(String string) throws IOException {
+    URL url=new URL(string);
+        HttpURLConnection connection=(HttpURLConnection) url.openConnection();
+        connection.connect();
+        InputStream stream=connection.getInputStream();
+        BufferedReader reader=new BufferedReader(new InputStreamReader(stream));
+   StringBuilder builder=new StringBuilder();
+   String line ="";
+   while((line=reader.readLine())!=null){
+builder.append(line);
+   }
+   String data=builder.toString();
+   reader.close();
+   return data;
+    }
+
+    private class ParserTask extends AsyncTask<String,Integer,List<HashMap<String,String>>>{
+        @Override
+        protected List<HashMap<String, String>> doInBackground(String... strings) {
+            JsonParser jsonParser=new JsonParser();
+            List<HashMap<String,String>> mapList=null;
+            JSONObject object=null;
+            try {
+                 object=new JSONObject(strings[0]);
+            mapList=jsonParser.parseResult(object);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            return mapList;
+        }
+
+        @Override
+        protected void onPostExecute(List<HashMap<String, String>> hashMaps) {
+           map.clear();
+           for(int i=0;i<hashMaps.size();i++){
+               HashMap<String,String> hashMapList=hashMaps.get(i);
+               double lat=Double.parseDouble(hashMapList.get("lat"));
+               double lng=Double.parseDouble(hashMapList.get("lng"));
+          String name=hashMapList.get("name");
+          LatLng latLng=new LatLng(lat,lng);
+          MarkerOptions options=new MarkerOptions();
+           options.position(latLng);
+          options.title(name);
+          map.addMarker(options);
+           }
+        }
+    }
 }
